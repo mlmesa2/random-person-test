b695218e85e7a1d88c5893daf869357c78d04eef
mlmesa <mlmesa@protonmail.com>
2023-07-23 15:35:52 -0400

    Initial commit

---
+++ app/src/main/java/com/test/randomuser/AppRandomUser.java
@@ -0,0 +1,9 @@
+package com.test.randomuser;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class AppRandomUser extends Application{
+}
---
+++ app/src/main/java/com/test/randomuser/Util.java
@@ -0,0 +1,5 @@
+package com.test.randomuser;
+
+public class Util {
+    static final String MAIN_URL = "http://localhost";
+}
